00441823973702a836fb72f3b03b53d65615a953
kth232 <kth7537@naver.com>
2024-06-10 11:11:30 +0900

    수정

--- src/textbook/chapter05/MyDate.java
+++ src/textbook/chapter05/MyDate.java
@@ -28,14 +28,14 @@ public class MyDate {
     public void setDay(int day) {
         switch(month){// month 값을 가지고 검증하기 때문에 set 순서 중요함!
             case 1, 3, 5, 7, 8, 10, 12: //해당 월일 때 1-31일까지
-                if (day < 0 || day > 31) {
+                if (day < 1 || day > 31) {
                     isValid = false;
                 } else {
                     this.day = day;
                 }
                 break;
             case 4, 6, 9, 11: //해당 월일 때 1-30일까지
-                if (day < 0 || day > 30) {
+                if (day < 1 || day > 30) {
                     isValid = false;
                 } else {
                     this.day = day;
@@ -44,13 +44,13 @@ public class MyDate {
             case 2:
                 //윤년 계산: 4로 나누어 떨어지고 100으로 나누어 떨어지지 않는 해, 400으로 나누어 떨어지는 해
                 if (( year % 4 == 0 && year % 100 != 0 ) || year % 400 == 0) {
-                    if (day < 0 || day > 29){ //윤년 2월은 29일까지
+                    if (day < 1 || day > 29){ //윤년 2월은 29일까지
                         isValid = false;
                     } else {
                         this.day = day;
                     }
                 } else {
-                    if (day < 0 || day > 28){ //평년 2월은 28일까지
+                    if (day < 1 || day > 28){ //평년 2월은 28일까지
                         isValid = false;
                     } else {
                         this.day = day;
