7657ce88abecc1565464fdf754af5ef8b19e631e
kth232 <kth7537@naver.com>
2024-06-17 09:05:08 +0900

    수정

--- src/textbook/program/GradeTest.java
+++ src/textbook/program/GradeTest.java
@@ -31,44 +31,44 @@ public class GradeTest {
     }
 
     public void createStudent() throws FileNotFoundException {
-        Scanner scanner = new Scanner(new FileInputStream("studentinfo.txt"));
         try {
-            throw new FileNotFoundException();
+            Scanner scanner = new Scanner(new FileInputStream("studentinfo.txt"));
+            String name;
+            int id;
+            int koreanScore;
+            int mathScore;
+            int majorCode;
+
+            Student student = null;
+
+            while(scanner.hasNextLine()) {
+                name = scanner.next();
+                id = scanner.nextInt();
+                koreanScore = scanner.nextInt();
+                mathScore = scanner.nextInt();
+                majorCode = scanner.nextInt();
+
+                if(majorCode == Define.KOREAN) {
+                    student = new Student(id, name, korean );
+                }
+                else if(majorCode == Define.MATH) {
+                    student = new Student(id, name, math );
+                }
+                else {
+                    System.out.println("전공 과목 오류");
+                    return;
+                }
+                goodSchool.addStudent(student);
+                korean.register(student);
+                math.register(student);
+                addScoreForStudent(student, korean, koreanScore);
+                addScoreForStudent(student, math, mathScore);
+            }
+            scanner.close();
+           // throw new FileNotFoundException();
         } catch (FileNotFoundException e){
             System.err.println("파일을 찾을 수 없습니다: " + e.getMessage());
         }
-        String name;
-        int id;
-        int koreanScore;
-        int mathScore;
-        int majorCode;
-
-        Student student = null;
-
-        while(scanner.hasNextLine()) {
-            name = scanner.next();
-            id = scanner.nextInt();
-            koreanScore = scanner.nextInt();
-            mathScore = scanner.nextInt();
-            majorCode = scanner.nextInt();
-
-            if(majorCode == Define.KOREAN) {
-                student = new Student(id, name, korean );
-            }
-            else if(majorCode == Define.MATH) {
-                student = new Student(id, name, math );
-            }
-            else {
-                System.out.println("전공 과목 오류");
-                return;
-            }
-            goodSchool.addStudent(student);
-            korean.register(student);
-            math.register(student);
-            addScoreForStudent(student, korean, koreanScore);
-            addScoreForStudent(student, math, mathScore);
-        }
-        scanner.close();
     }
 
     //과목별 성적 입력
